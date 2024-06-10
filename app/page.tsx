a66f98f86577df0c4883dee92e2d55ea972c3f8c
douid <douidriss@gmail.com>
2024-06-10 15:32:58 +0100

    bucket2

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Register from "@/components/Register";
 import Funco from "@/components/Funco";
 import crypto from "crypto";
-import { Bucket } from "sst/node/bucket";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-
+// Function to fetch the signed URL
 async function fetchSignedUrl() {
   const command = new PutObjectCommand({
     ACL: "public-read",
@@ -16,8 +15,10 @@ async function fetchSignedUrl() {
   return url;
 }
 
+// Main Page Component
+export default async function Home() {
+  const url = await fetchSignedUrl();
 
-export default function Home({ url }: { url: string }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Register />
