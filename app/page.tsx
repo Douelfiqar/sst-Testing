955d27e32245927f4820ba7e021c461895487743
douid <douidriss@gmail.com>
2024-06-10 15:24:42 +0100

    bucket

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 import Register from "@/components/Register";
+import Funco from "@/components/Funco";
+import crypto from "crypto";
+import { Bucket } from "sst/node/bucket";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-export default function Home() {
+
+
+export default function Home({ url }: { url: string }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Register />
-      
+      <Funco url={url} />
     </main>
   );
+}
+
+export async function getServerSideProps() {
+  const command = new PutObjectCommand({
+    ACL: "public-read",
+    Key: crypto.randomUUID(),
+    Bucket: process.env.NEXT_PUBLIC_BUCKET_NAME!,
+  });
+  const url = await getSignedUrl(new S3Client({}), command);
+
+  return { props: { url } };
 }
