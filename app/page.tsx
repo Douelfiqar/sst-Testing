80dfd4a940780e65ef1492b270b0bac8f33517bc
douid <douidriss@gmail.com>
2024-06-10 15:11:55 +0100

    second

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,10 @@
-// components/Home.tsx
-import crypto from "crypto";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import Register from "@/components/Register";
 
-async function generateSignedUrl() {
-  const bucketName = process.env.NEXT_PUBLIC_BUCKET_NAME;
-  if (!bucketName) {
-    throw new Error("Bucket name not set in environment variables");
-  }
-
-  const command = new PutObjectCommand({
-    ACL: "public-read",
-    Key: crypto.randomUUID(),
-    Bucket: bucketName,
-  });
-  const url = await getSignedUrl(new S3Client({}), command);
-  return url;
-}
-
-export default async function Home() {
-  const url = await generateSignedUrl();
-
+export default function Home() {
   return (
-    <main>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-
-          const file = (e.target as HTMLFormElement).file.files?.[0]!;
-
-          const image = await fetch(url, {
-            body: file,
-            method: "PUT",
-            headers: {
-              "Content-Type": file.type,
-              "Content-Disposition": `attachment; filename="${file.name}"`,
-            },
-          });
-
-          window.location.href = image.url.split("?")[0];
-        }}
-      >
-        <input name="file" type="file" accept="image/png, image/jpeg" />
-        <button type="submit">Upload</button>
-      </form>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Register />
+      
     </main>
   );
 }
